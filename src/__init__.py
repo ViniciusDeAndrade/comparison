dd8d2d63d4e2dd37258ded31866a9af5a80d310d
Vinicius de Andrade <viniciusclo@gmail.com>
2024-06-01 16:19:35 +0100

    feat: first commit and endpoint

---
+++ src/__init__.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+import numpy as np
+import cv2
+from PIL import Image
+import pytesseract
+from io import BytesIO
+from skimage.metrics import structural_similarity as compare_ssim
+
+import os
+print(os.getenv("PATH"))
+
+app = FastAPI()
+
+def calculate_image_similarity(img1, img2):
+    img1_gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    img2_gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    img1_gray = cv2.resize(img1_gray, (256, 256))
+    img2_gray = cv2.resize(img2_gray, (256, 256))
+    score, _ = compare_ssim(img1_gray, img2_gray, full=True)
+    return score
+
+def extract_text_from_image(image):
+    return pytesseract.image_to_string(image)
+
+@app.post("/compare-images/")
+async def compare_and_extract(file1: UploadFile = File(...), file2: UploadFile = File(...)):
+    img1 = Image.open(BytesIO(await file1.read()))
+    img2 = Image.open(BytesIO(await file2.read()))
+
+    img1_array = np.array(img1)
+    img2_array = np.array(img2)
+
+    similarity_score = calculate_image_similarity(img1_array, img2_array)
+    text1 = extract_text_from_image(img1)
+    text2 = extract_text_from_image(img2)
+
+    return JSONResponse(content={
+        "similarity_score": similarity_score,
+        "text1": text1,
+        "text2": text2
+    })
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
